7ff538574f5b5fc28201ea92c6024870089e3b67
Allan Chaves <allancac@gmail.com>
2023-05-16 06:56:19 -0300

    Variáveis de Ambiente Conexao Sequelize

--- 003_backend_project/src/config/database.js
+++ 003_backend_project/src/config/database.js
@@ -1,7 +1,11 @@
 // Configuração do banco de dados
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('EMASA', 'DFEEMS001', 'Ubuntu@20', {
+const SCHEMA = process.env.SCHEMA || 'EMASA'
+const user = /*process.env.USER || */'DFEEMS001'
+const password = process.env.PASSWORD || 'Ubuntu@20'
+
+const sequelize = new Sequelize(SCHEMA, user, password, {
   host: 'localhost',
   dialect: 'mysql',
   define: {
