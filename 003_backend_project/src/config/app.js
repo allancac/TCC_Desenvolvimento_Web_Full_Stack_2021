813de426437d06910d40a4fda0e3503b9667230b
Allan Cezar Almeida Chaves <allancac@gmail.com>
2023-06-29 15:31:11 -0300

    Modificação APP.js
    Alteração para aplicar as injeções de dependências nas camadas da aplicação.

--- 003_backend_project/src/config/app.js
+++ 003_backend_project/src/config/app.js
@@ -1,38 +1,74 @@
 const express = require('express');
 const app = express();
-const Models = require('../models/index');
+const Models = require('../models/index');  //Classe criadora de modelos
 
 const configureApp = async (database) => {
-
   // Definição de todos os modelos do sistema
   const Modelos = new Models(database)
-  const clienteModel = Modelos.getClienteModel();
-  const enderecoModel = Modelos.getEnderecoModel();
-  const veiculoModel = Modelos.getVeiculoModel();
-  const motoristaModel = Modelos.getMotoristaModel();
+
+  // Classes dos Modelos que representam as entidades do sistema.
+  const ClienteModel = Modelos.getClienteModel();
+  const EnderecoModel = Modelos.getEnderecoModel();
+  const VeiculoModel = Modelos.getVeiculoModel();
+  const MotoristaModel = Modelos.getMotoristaModel();
+  const VendaModel = Modelos.getVendaModel();
+  const ProdutoModel = Modelos.getProdutoModel();
+  const EstoqueModel = Modelos.getEstoqueModel();
+  const UsuarioModel = Modelos.getUsuarioModel();
+
 
   // Injeção das dependências nas camadas da API
-  const motoristaService = require('../services/motoristaService')(motoristaModel);
-  const motoristaController = require('../controllers/motoristaController')(motoristaService)
-  const motoristaRoutes = require('../routes/motoristaRoutes')(motoristaController);
-
-  const clienteService = require('../services/clienteService')(clienteModel);
+  //  CLIENTE
+  const clienteService = require('../services/clienteService')(ClienteModel);
   const clienteController = require('../controllers/clienteController')(clienteService)
   const clienteRoutes = require('../routes/clienteRoutes')(clienteController);
 
-  const enderecoService = require('../services/enderecoService')(enderecoModel);
+  //  ENDEREÇO
+  const enderecoService = require('../services/enderecoService')(EnderecoModel);
   const enderecoController = require('../controllers/enderecoController')(enderecoService)
   const enderecoRoutes = require('../routes/enderecoRoutes')(enderecoController);
 
-  const veiculoService = require('../services/veiculosService')(veiculoModel);
+  //  MOTORISTA
+  const motoristaService = require('../services/motoristaService')(MotoristaModel);
+  const motoristaController = require('../controllers/motoristaController')(motoristaService)
+  const motoristaRoutes = require('../routes/motoristaRoutes')(motoristaController);
+
+  //  VEÍCULO
+  const veiculoService = require('../services/veiculosService')(VeiculoModel);
   const veiculoController = require('../controllers/veiculoController')(veiculoService)
   const veiculoRoutes = require('../routes/veiculoRoutes')(veiculoController);
+
+  
+  // PRODUTO
+  const produtoService = require('../services/produtoService')(ProdutoModel);
+  const produtoController = require('../controllers/produtoController')(produtoService)
+  const produtoRoutes = require('../routes/produtoRoutes')(produtoController);
+
+  // ESTOQUE
+  const estoqueService = require('../services/estoqueService')(EstoqueModel);
+  const estoqueController = require('../controllers/estoqueController')(estoqueService)
+  const estoqueRoutes = require('../routes/estoqueRoutes')(estoqueController);
+  
+  // USUÁRIO
+  const usuarioService = require('../services/usuarioService')(UsuarioModel);
+  const usuarioController = require('../controllers/usuarioController')(usuarioService)
+  const usuarioRoutes = require('../routes/usuarioRoutes')(usuarioController);
+  
+    //  VENDA
+    const vendaService = require('../services/vendaService')(VendaModel);
+    const vendaController = require('../controllers/vendaController')(vendaService)
+    const vendaRoutes = require('../routes/vendaRoutes')(vendaController);
+  
 
 
 
   // Configuração do body parser de Express para receber dados no formato JSON
   app.use(express.json());
-
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
   // Configuração das rotas
   app.use(
     [
@@ -40,6 +76,10 @@ const configureApp = async (database) => {
       clienteRoutes,
       enderecoRoutes,
       veiculoRoutes,
+      vendaRoutes,
+      produtoRoutes,
+      estoqueRoutes,
+      usuarioRoutes
     ]
   )
   // Retorna uma instância do Express com os Routers 
