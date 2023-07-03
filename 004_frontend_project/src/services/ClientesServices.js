64a7cba3422fb889b56cc3bab9aeccb0f29c737c
Allan Cezar Almeida Chaves <allancac@gmail.com>
2023-07-03 14:26:17 -0300

    Correções do serviço do Cliente no React
    Correções nos Serviços de consulta do APP React à API

--- 004_frontend_project/src/services/ClientesServices.js
+++ 004_frontend_project/src/services/ClientesServices.js
@@ -1,18 +1,14 @@
 import axios from 'axios';
-const serverName = 'virtserver.swaggerhub.com/'
-const path = 'allancac/vendas_emasa/1.0.0'
+import config from './config'
 
 class ClientesServices {
   constructor() {
-    this.serverName = serverName;
-    this.axiosInstance = axios.create({
-      baseURL: `https://${this.serverName}${path}`
-    });
+    this.axiosInstance = axios.create({ baseURL: `${config.baseURL}` });
   }
 
-  async enviarDadosCliente(cliente) {
+  async buscarListaClientes(offset = 0, limit = 20) {
     try {
-      const response = await this.axiosInstance.post('/clientes', cliente);
+      const response = await this.axiosInstance.get(`/clientes/?offset=${offset}&limit=${limit}`);
       return response.data;
     } catch (error) {
       console.error(error);
@@ -20,9 +16,43 @@ class ClientesServices {
     }
   }
 
-  async buscarListaClientes() {
+  async buscarCliente(id) {
     try {
-      const response = await this.axiosInstance.get('/clientes/?bodyLimit=20&pagelimit=10');
+      const response = await this.axiosInstance.get(`/clientes/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async enviarDadosCliente(dadosCliente) {
+    try {
+      const response = await this.axiosInstance.post(`/clientes`, dadosCliente);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+
+  async atualizarDadosCliente(id, dadosCliente) {
+
+    try {
+      const response = await this.axiosInstance.put(`/clientes/${id}`, dadosCliente);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async deletarCliente(id) {
+    try {
+      console.log(id)
+      const response = await this.axiosInstance.delete(`/clientes/${id}`);
+      console.log(response);
       return response.data;
     } catch (error) {
       console.error(error);
