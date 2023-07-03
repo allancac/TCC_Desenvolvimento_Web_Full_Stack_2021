97502d686a37cc8ecaf0526278c070fef5e66b72
Allan Cezar Almeida Chaves <allancac@gmail.com>
2023-07-03 14:41:34 -0300

    Criação do componente React de paginação

---
+++ 004_frontend_project/src/components/Paginacao/Paginacao.jsx
@@ -0,0 +1,36 @@
+import { Pagination } from 'react-bootstrap';
+
+export const Paginacao = ({ total, limit, offset, alteraPagina }) => {
+  const paginas = Math.ceil(total / limit);
+
+  const handleClick = (pagina) => {
+    const newOffset = (pagina - 1) * limit;
+    alteraPagina(newOffset);
+  };
+
+  return (
+    <Pagination>
+      <Pagination.First onClick={() => handleClick(1)} />
+      <Pagination.Prev
+        onClick={() => handleClick(Math.max(1, Math.ceil(offset / limit)))}
+      />
+      {Array.from({ length: paginas }, (_, index) => index + 1).map(
+        (pagina) => (
+          <Pagination.Item
+            key={pagina}
+            active={offset === (pagina - 1) * limit}
+            onClick={() => handleClick(pagina)}
+          >
+            {pagina}
+          </Pagination.Item>
+        )
+      )}
+      <Pagination.Next
+        onClick={() =>
+          handleClick(Math.min(paginas, Math.ceil(offset / limit) + 1))
+        }
+      />
+      <Pagination.Last onClick={() => handleClick(paginas)} />
+    </Pagination>
+  );
+};
