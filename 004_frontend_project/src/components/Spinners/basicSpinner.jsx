d5fcd08e41a4f9435a777ba5196a684b5c9f2c82
Allan Cezar Almeida Chaves <allancac@gmail.com>
2023-07-03 16:44:03 -0300

    Criação do Componente Spinner

---
+++ 004_frontend_project/src/components/Spinners/basicSpinner.jsx
@@ -0,0 +1,7 @@
+import Spinner from 'react-bootstrap/Spinner';
+
+function BasicSpinner() {
+  return <Spinner animation='border' role='status' variant='primary' />;
+}
+
+export default BasicSpinner;
