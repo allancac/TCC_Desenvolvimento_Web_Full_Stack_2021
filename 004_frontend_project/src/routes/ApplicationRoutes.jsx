219f5ae519a898a8a04ef74a02234008ebcd615e
Allan Cezar Almeida Chaves <allancac@gmail.com>
2023-07-03 14:23:32 -0300

    Definições das Rotas do React

---
+++ 004_frontend_project/src/routes/ApplicationRoutes.jsx
@@ -0,0 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
+import { Main } from '../views/main';
+import { NotFound } from '../views/notFound.jsx';
+import ClientesRoutes from './clientes/ClientesRoutes';
+import VeiculosRoutes from './veiculos/VeiculosRoutes';
+import MotorisasRoutes from './motoristas/MotoristasRoutes';
+import ProdutosRoutes from './produtos/ProdutosRoutes';
+import EstoquesRoutes from './estoques/EstoquesRoutes';
+import VendasRoutes from './vendas/VendasRoutes';
+import UsuariosRoutes from './usuarios/UsuariosRoutes';
+
+export const ApplicationRoutes = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Main />} />
+      <Route path='*' element={<NotFound />} />
+
+      <Route path='/clientes/*' element={<ClientesRoutes />} />
+      <Route path='/veiculos/*' element={<VeiculosRoutes />} />
+      <Route path='/motoristas/*' element={<MotorisasRoutes />} />
+      <Route path='/produtos/*' element={<ProdutosRoutes />} />
+      <Route path='/estoques/*' element={<EstoquesRoutes />} />
+      <Route path='/vendas/*' element={<VendasRoutes />} />
+      <Route path='/usuarios/*' element={<UsuariosRoutes />} />
+    </Routes>
+  );
+};
