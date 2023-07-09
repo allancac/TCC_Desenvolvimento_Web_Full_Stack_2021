59561379b9233a0583091e6f443f57893d2e7aae
Allan Chaves <allancac@gmail.com>
2023-07-09 00:20:16 -0300

    Criação das Variáveis de ambiente para o Database e Porta

--- 003_backend_project/src/config/database.js
+++ 003_backend_project/src/config/database.js
@@ -1,12 +1,14 @@
+require('dotenv').config(); // Carregar variáveis de ambiente
 // Configuração do banco de dados
 const { Sequelize } = require('sequelize');
-const schema = process.env.SCHEMA || 'EMASA'
-const dbUser = process.env.DBUSER || 'DFEEMS001'
-const password = process.env.PASSWORD || 'Ubuntu@20'
+const schema = process.env.DBSCHEMA
+const dbUser = process.env.DBUSER
+const password = process.env.DBPASSWORD
+const dbHost = process.env.DBHOST
 
 const configureDatabase = async () => {
   const sequelize = new Sequelize(schema, dbUser, password, {
-    host: 'localhost',
+    host: dbHost,
     dialect: 'mysql',
     timezone: '-03:00',
     define: {
