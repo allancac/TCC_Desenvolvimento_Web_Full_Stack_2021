37605e1a045d1e6db47040779511648c101a0241
Allan Chaves <allancac@gmail.com>
2023-07-17 20:41:25 -0300

    Alterações nos arquivos DMLs de Inserções

--- 002_Diagramas/Inserts - Usuários.sql
+++ 002_Diagramas/Inserts - Usuários.sql
@@ -1,2 +1,14 @@
-INSERT INTO EMASA.USUARIOs(id,nome,perfil,data_registro)
-VALUES(1,'Allan Chaves', 'administrador', current_timestamp);
+INSERT INTO EMASA.Usuarios(ativo, email, senha, nome, sobrenome, perfil, createdAt, updatedAt) 
+VALUES(false,  'allancac@gmail.com', 'secret', 'Allan', 'Chaves', 'vendedor',current_timestamp,current_timestamp);
+
+#SELECT * from 
+#EMASA.Usuarios
+#where ativo = false;
+
+#UPDATE EMASA.Usuarios
+#set ativo = true
+#where id = 2;
+
+#delete from 
+#EMASA.Usuarios
+#where id = 1;
--- 002_Diagramas/Inserts - Vendas.sql
+++ 002_Diagramas/Inserts - Vendas.sql
@@ -1,2 +1,8 @@
-INSERT INTO EMASA.Vendas(id_produto,id_estoque,id_cliente,id_endereco,cpf_motorista,placa,quantidade,preco_total,createdAt,updatedAt)
-VALUES(1,1,4,4,'12345678902','ABC1234',12,840,current_timestamp,current_timestamp);
+INSERT INTO EMASA.Vendas(id_usuario, id_produto,id_estoque,id_cliente,id_endereco,cpf_motorista,placa,quantidade,preco_total,createdAt,updatedAt)
+VALUES(2,1,1,4,4,'12345678902','ABC1234',12,840,current_timestamp,current_timestamp);
+
+INSERT INTO EMASA.Vendas(id_usuario, id_produto,id_estoque,id_cliente,id_endereco,cpf_motorista,placa,quantidade,preco_total,createdAt,updatedAt)
+VALUES(1,1,1,4,4,'12345678902','ABC1234',10,700,current_timestamp,current_timestamp);
+
+
+SELECT * FROM EMASA.Vendas;
