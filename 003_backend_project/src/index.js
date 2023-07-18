68b5a6aeba7c3cf94ac133b1b00111987bf9674b
Allan Chaves <allancac@gmail.com>
2023-07-18 19:01:01 -0300

    Instalação do os e do cross-env e Alteração index.js

--- 003_backend_project/src/index.js
+++ 003_backend_project/src/index.js
@@ -1,8 +1,8 @@
 //  FIXME: Alerar todas importações de módulos de require para o import(ES6)
 require('dotenv').config(); // Carregar variáveis de ambiente
+const os = require('os')
 const { configureApp } = require('./config/app')
 const configureDatabase = require('./config/database')
-
 
 // Sincronização do modelo com o banco de dados
 const startServer = async () => {
@@ -13,24 +13,26 @@ const startServer = async () => {
     // Configuração das rotas utilizdas no aplicativo
     const app = await configureApp(connection);
 
-    try {
+        try {
       await connection.authenticate();
-      console.log('Conexão estabelecida com sucesso.');
+      console.log(`Conexão com o Banco de Dados ${connection.config.host}:${connection.config.port} estabelecida com sucesso.`);
       await connection.sync(
         {
           // alter: true,
           // force: true
         }
       );
-      console.log('Os Modelos foram sincronizados com o banco de dados.');
+      console.log('Os Modelos foram sincronizados com o Banco de Dados.');
     } catch (error) {
       console.error('Erro ao conectar e sincronizar:', error);
       process.exit(1);
     }
     // Inicialização do servidor
-    const PORT = process.env.PORT || 5500;
-    app.listen(PORT, () => {
-      console.log(`Servidor rodando na porta ${PORT}`);
+    const port = process.env.PORT || 5500;
+    const hostname = os.hostname
+    const networkInterfaces = os.networkInterfaces();
+    app.listen(port, () => {
+      console.log(`Servidor rodando em ambiente "${process.env.NODE_ENV}" no host ${hostname} - ${networkInterfaces['eth0'][0].address}:${port}`);
     });
   } catch (error) {
     console.error('Erro ao iniciar o servidor:', error);
