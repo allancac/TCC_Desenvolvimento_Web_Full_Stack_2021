8925c3df739756ded632dd73f8c45bddaad70e65
Allan Chaves <allancac@gmail.com>
2023-07-18 10:23:23 -0300

    Header - Alteração do componente Header

--- 004_frontend_project/src/components/Header/index.jsx
+++ 004_frontend_project/src/components/Header/index.jsx
@@ -1,61 +1,70 @@
-import { Container, Nav, Navbar, NavDropdown, Row } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import {
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Row,
+  DropdownButton,
+  Dropdown,
+} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 export const Header = () => {
   return (
-    <Navbar bg='secondary' variant='dark' expand='md' sticky='top'>
+    <Navbar bg="secondary" variant="dark" expand="md" sticky="top">
       <Container>
-        <LinkContainer to='/'>
+        <LinkContainer to="/">
           <Navbar.Brand>MINERADORA S.A.</Navbar.Brand>
         </LinkContainer>
 
-        <Navbar.Toggle aria-controls='navbarScroll' />
+        <Navbar.Toggle aria-controls="navbarScroll" />
 
-        <Navbar.Collapse id='navbarScroll'>
+        <Navbar.Collapse id="navbarScroll">
           <Nav
-            className='me-auto my-2 my-lg-0'
-            style={{ maxHeight: '350px' }}
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "350px" }}
             navbarScroll
           >
-            <LinkContainer to='/vendas'>
+            <LinkContainer to="/vendas">
               <Nav.Link>Vendas</Nav.Link>
             </LinkContainer>
-            <LinkContainer to='/clientes'>
+            <LinkContainer to="/clientes">
               <Nav.Link>Clientes</Nav.Link>
             </LinkContainer>
-            <LinkContainer to='/veiculos'>
+            <LinkContainer to="/veiculos">
               <Nav.Link>Veículos</Nav.Link>
             </LinkContainer>
-            <LinkContainer to='/motoristas'>
+            <LinkContainer to="/motoristas">
               <Nav.Link>Motoristas</Nav.Link>
             </LinkContainer>
-            <LinkContainer to='/produtos'>
+            <LinkContainer to="/produtos">
               <Nav.Link>Produtos</Nav.Link>
             </LinkContainer>
-            <LinkContainer to='/estoques'>
+            <LinkContainer to="/estoques">
               <Nav.Link>Estoques</Nav.Link>
             </LinkContainer>
-            <LinkContainer to='/usuarios'>
+            <LinkContainer to="/usuarios">
               <Nav.Link>Usuários</Nav.Link>
             </LinkContainer>
-            <LinkContainer to='/relatorios'>
+            <LinkContainer to="/relatorios">
               <Nav.Link disabled>Relatórios</Nav.Link>
             </LinkContainer>
           </Nav>
 
-          <Nav>
-            <NavDropdown
-              className='justify-content-end'
-              title='Usuário'
-              id='navbarScrollingDropdown'
-            >
-              <NavDropdown.Item href='#action3'>
-                Mais informações
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href='#action5'>Sair</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+          <DropdownButton
+            id="dropdown-button-dark"
+            variant="secondary"
+            title="Usuário"
+            data-bs-theme="dark"
+          >
+            <Dropdown.Item>
+              <LinkContainer to="/login">
+                <Nav.Link >Entrar</Nav.Link>
+              </LinkContainer>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sair</Dropdown.Item>
+          </DropdownButton>
         </Navbar.Collapse>
       </Container>
     </Navbar>
