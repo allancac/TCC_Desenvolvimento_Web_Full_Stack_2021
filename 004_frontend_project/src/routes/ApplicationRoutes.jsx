466969ebc54cd32363839b6bf1b162c5b52295d9
Allan Chaves <allancac@gmail.com>
2023-07-18 10:27:37 -0300

    ApplicationRoutes - Adicção de novas rotas

--- 004_frontend_project/src/routes/ApplicationRoutes.jsx
+++ 004_frontend_project/src/routes/ApplicationRoutes.jsx
@@ -1,27 +1,29 @@
-import { Route, Routes } from 'react-router-dom';
-import { Main } from '../views/main';
-import { NotFound } from '../views/notFound.jsx';
-import ClientesRoutes from './clientes/ClientesRoutes';
-import VeiculosRoutes from './veiculos/VeiculosRoutes';
-import MotorisasRoutes from './motoristas/MotoristasRoutes';
-import ProdutosRoutes from './produtos/ProdutosRoutes';
-import EstoquesRoutes from './estoques/EstoquesRoutes';
-import VendasRoutes from './vendas/VendasRoutes';
-import UsuariosRoutes from './usuarios/UsuariosRoutes';
+import { Route, Routes } from "react-router-dom";
+import { Main } from "../views/main";
+import { NotFound } from "../views/notFound.jsx";
+import ClientesRoutes from "./clientes/ClientesRoutes";
+import VeiculosRoutes from "./veiculos/VeiculosRoutes";
+import MotorisasRoutes from "./motoristas/MotoristasRoutes";
+import ProdutosRoutes from "./produtos/ProdutosRoutes";
+import EstoquesRoutes from "./estoques/EstoquesRoutes";
+import VendasRoutes from "./vendas/VendasRoutes";
+import UsuariosRoutes from "./usuarios/UsuariosRoutes";
+import {Signin} from "../views/signin/index";
 
 export const ApplicationRoutes = () => {
   return (
     <Routes>
-      <Route path='/' element={<Main />} />
-      <Route path='*' element={<NotFound />} />
+      <Route path="/" element={<Main />} />
+      <Route path="/login" element={<Signin />} />
+      <Route path="*" element={<NotFound />} />
 
-      <Route path='/clientes/*' element={<ClientesRoutes />} />
-      <Route path='/veiculos/*' element={<VeiculosRoutes />} />
-      <Route path='/motoristas/*' element={<MotorisasRoutes />} />
-      <Route path='/produtos/*' element={<ProdutosRoutes />} />
-      <Route path='/estoques/*' element={<EstoquesRoutes />} />
-      <Route path='/vendas/*' element={<VendasRoutes />} />
-      <Route path='/usuarios/*' element={<UsuariosRoutes />} />
+      <Route path="/clientes/*" element={<ClientesRoutes />} />
+      <Route path="/veiculos/*" element={<VeiculosRoutes />} />
+      <Route path="/motoristas/*" element={<MotorisasRoutes />} />
+      <Route path="/produtos/*" element={<ProdutosRoutes />} />
+      <Route path="/estoques/*" element={<EstoquesRoutes />} />
+      <Route path="/vendas/*" element={<VendasRoutes />} />
+      <Route path="/usuarios/*" element={<UsuariosRoutes />} />
     </Routes>
   );
 };
