ceb1de06d7be8c0f048df2c54cd20180131f1c24
Allan Chaves <allancac@gmail.com>
2023-07-20 21:42:06 -0300

    Criação da Var de estado User na store do Redux

--- 004_frontend_project/src/redux/reducers/index.js
+++ 004_frontend_project/src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { vendaReducer } from './vendasReducers';
 import { menuLateralReducer } from './menuLateralReducers';
+import { userReducer } from './userReducer';
 
 export default combineReducers({
   venda: vendaReducer,
   menuLateral: menuLateralReducer,
+  user: userReducer,
   devTools: process.env.NODE_ENV !== 'production',
 });
